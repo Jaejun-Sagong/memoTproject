28279d438fc2fb29be262ba2d48fb927c0df8655
ChHong <pnci1029@gmail.com>
2022-08-06 20:16:00 +0900

    s3

--- memoTproject/src/main/java/com/sparta/memoproject/service/MemoService.java
+++ memoTproject/src/main/java/com/sparta/memoproject/service/MemoService.java
@@ -1,16 +1,25 @@
 package com.sparta.memoproject.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.sparta.memoproject.dto.MemoRequestDto;
 import com.sparta.memoproject.model.Member;
 import com.sparta.memoproject.model.Memo;
 import com.sparta.memoproject.repository.MemberRepository;
 import com.sparta.memoproject.repository.MemoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor //final로 선언한 변수가 있으면 꼭 생성해달라는 것
 @Service
@@ -19,6 +28,12 @@ public class MemoService {
 
     private final MemoRepository memoRepository; // [2번]update메소드 작성 전에 id에 맞는 값을 찾으려면 find를 써야하는데 find를 쓰기위해서는 Repository가 있어야한다.
     private final MemberRepository memberRepository;
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;  // S3 버킷 이름
+
 
     public String getNickname() {
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -40,8 +55,9 @@ public class MemoService {
     }
 
     @Transactional
-    public Memo creatMemo(MemoRequestDto requestDto, String nickName) {
-        Memo memo = new Memo(requestDto, nickName);
+    public Memo creatMemo(MemoRequestDto requestDto, String nickName, String urlPath)  {
+        Memo memo = new Memo(requestDto, nickName, urlPath);
+        System.out.println(urlPath);
 
         memoRepository.save(memo);
 
@@ -59,4 +75,6 @@ public class MemoService {
         memoRepository.deleteById(id);
         return true;
     }
+
+
 }
